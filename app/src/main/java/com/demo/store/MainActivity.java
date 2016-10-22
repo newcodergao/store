bd22334098bf0d40577510a85c683472bc25b3d2
gsj <nusthyg@qq.com>
2016-10-22 11:19:55 +0800

    444

--- app/src/main/java/com/demo/store/MainActivity.java
+++ app/src/main/java/com/demo/store/MainActivity.java
@@ -1,13 +1,66 @@
 package com.demo.store;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+import com.demo.store.widget.PagerTab;
+
+public class MainActivity extends BaseActivity {
+
+    private PagerTab mPagerTab;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void initView() {
         setContentView(R.layout.activity_main);
+        mPagerTab = (PagerTab) findViewById(R.id.tabs);
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+    }
+    private class MainAdapter extends FragmentPagerAdapter{
+
+        public MainAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            return super.instantiateItem(container, position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            super.destroyItem(container, position, object);
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return super.isViewFromObject(view, object);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return super.getPageTitle(position);
+        }
     }
 }
